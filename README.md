49ddaaf892d6030e91b69a217bcbe0c3883ac8bb
Alan Plum <me@pluma.io>
2013-12-29 14:00:14 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,136 @@
+# Synopsis
+
+**httperr** provides Error factories for all HTTP error status codes.
+
+[![Build Status](https://travis-ci.org/pluma/httperr.png?branch=master)](https://travis-ci.org/pluma/httperr) [![NPM version](https://badge.fury.io/js/httperr.png)](http://badge.fury.io/js/httperr) [![Dependencies](https://david-dm.org/pluma/httperr.png)](https://david-dm.org/pluma/httperr)
+
+# Why?
+
+There are several libraries that already do this, but most of them either only support a very limited number of status codes, don't capture stack traces correctly or are no longer maintained.
+
+The biggest difference in **httperr** is that it lets you attach relevant information for the error in a single function call, allowing you to separate your error handling and error response logic without losing the semantics of HTTP status codes.
+
+# Install
+
+## With NPM
+
+```sh
+npm install httperr
+```
+
+## From source
+
+```sh
+git clone https://github.com/pluma/httperr.git
+cd httperr
+npm install
+make && make dist
+```
+
+# Basic usage example
+
+```javascript
+var httperr = require('httperr');
+
+var err = httperr[404]('The path "/example" could not be resolved');
+console.log(err);
+/*
+{ [NotFound: The path "/example" could not be resolved]
+  title: 'Not Found',
+  name: 'NotFound',
+  code: 'NOT_FOUND',
+  statusCode: 404,
+  message: 'The path "/example" could not be resolved'
+}
+*/
+throw err;
+/*
+NotFound: The path "/example" could not be resolved
+    at ...
+*/
+
+console.log(httperr.methodNotAllowed({allowed: ['GET', 'POST']}));
+/*
+{ [MethodNotAllowed]
+  title: 'Method Not Allowed',
+  name: 'MethodNotAllowed',
+  code: 'METHOD_NOT_ALLOWED',
+  statusCode: 405,
+  message: '',
+  allowed: ['GET', 'POST']
+}
+*/
+```
+
+# API
+
+# httperr.{errorName}([config:Object]):Error
+
+Creates an Error object.
+
+Example:
+
+```javascript
+httperr.notFound({message: 'That does not exist'});
+```
+
+If `config` is a string, it will be treated as `config.message`.
+
+If `config` is an object, it can have the following properties:
+
+## config.message (optional)
+A descriptive human-readable title describing the error's cause.
+
+## config.cause (optional)
+The underlying exception that caused the HTTP error.
+
+## config.details (optional)
+A detailed human-readable description of the error's cause and possible solutions.
+
+## config.allowed (optional)
+The methods allowed for this URL.
+
+This property is only available for `405 Method Not Allowed` errors and can be used to populate the `Allowed` header.
+
+## config.retryAfter (optional)
+The minimum delay before the request should be attempted again.
+
+This property is only available for `429 Too Many Requests` and `420 Enhance Your Calm` (Twitter API) errors and can be used to populate the `Retry-After` header.
+
+## config.parameters (optional)
+The parameters with which the request should be retried.
+
+This property is only available for `449 Retry With` (Microsoft) errors and can be used to populate the response status message.
+
+## config.location (optional)
+The location for which the request should be repeated.
+
+This property is only available for `451 Redirect` (Microsoft) errors and can be used to populate the proprietary `X-MS-Location` response header.
+
+# httperr.{statusCode}(config:Object):Error
+
+See above.
+
+Example:
+
+```javascript
+httperr[404]({message: 'That does not exist either'});
+```
+
+# httperr.createFactory(status, title, [init]):Function
+Creates a new factory function for the given HTTP error type.
+
+Takes the following arguments:
+
+## title
+A human-readable title for the HTTP error.
+
+## status
+The HTTP response status code for the HTTP error.
+
+## init (optional)
+A function which will be invoked as a method of the new error with the `config` argument immediately after the error is created by the factory. Can be used to process additional error-specific configuration parameters.
+
+# License
+
+The MIT/Expat license.
---
+++ index.js
@@ -0,0 +1,151 @@
+/*! httperr 0.1.0 Copyright (c) 2013 Alan Plum. MIT licensed. @preserve */
+exports.createFactory = createFactory;
+
+function createFactory(status, title, init) {
+  var simpleTitle = simplify(title);
+  var name = camelCase(simpleTitle);
+  var code = ucUnderscore(simpleTitle);
+  return function(config) {
+    if (!config) {
+      config = {};
+    } else if (typeof config === 'string') {
+      config = {message: config};
+    }
+    var err = new Error(config.message);
+    var stack = err.stack.split('\n');
+    stack.splice(1, 1); // httperr module
+    err.stack = stack.join('\n');
+    err.title = title;
+    err.name = name;
+    err.code = code;
+    err.statusCode = status;
+    err.cause = config.cause;
+    err.details = config.details;
+    Object.defineProperty(err, 'message', {
+      value: err.message,
+      configurable: true,
+      enumerable: true,
+      writable: true
+    });
+    if (typeof init === 'function') {
+      init.call(err, config);
+    }
+    return err;
+  };
+}
+
+function simplify(str) {
+  return str.replace(/^An?\s+|'/gi, '').replace(/-/g, ' ');
+}
+
+function ucUnderscore(str) {
+  return str.replace(/\s+/g, '_').toUpperCase();
+}
+
+function camelCase(str) {
+  return str.split(' ').map(titleCase).join('');
+}
+
+function snakeCase(str) {
+  var s = camelCase(str);
+  return s.slice(0, 1).toLowerCase() + s.slice(1);
+}
+
+function titleCase(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function spread(fn) {
+  return function(args) {
+    return fn.apply(this, args);
+  };
+}
+
+[
+  [400, 'Bad Request'],
+  [401, 'Unauthorized', function(config) {
+    this.authenticate = config.authenticate;
+  }],
+  [402, 'Payment Required'],
+  [403, 'Forbidden'],
+  [404, 'Not Found'],
+  [405, 'Method Not Allowed', function(config) {
+    this.allowed = config.allowed;
+  }],
+  [406, 'Not Acceptable'],
+  [407, 'Proxy Authentication Required', function(config) {
+    this.proxyAuthenticate = config.proxyAuthenticate;
+  }],
+  [408, 'Request Timeout'],
+  [409, 'Conflict'],
+  [410, 'Gone'],
+  [411, 'Length Required'],
+  [412, 'Precondition Failed'],
+  [413, 'Request Entity Too Large'],
+  [414, 'Request URI Too Long'],
+  [415, 'Unsupported Media Type'],
+  [416, 'Requested Range Not Satisfiable', function(config) {
+    this.contentRange = config.contentRange;
+  }],
+  [417, 'Expectation Failed'],
+  [418, "I'm a Teapot"],
+  [419, 'Authentication Timeout'],
+  [420, 'Enhance Your Calm', function(config) {
+    this.retryAfter = config.retryAfter;
+  }],
+  [422, 'Unprocessable Entity'],
+  [423, 'Locked'],
+  [424, 'Failed Dependency'],
+  [424, 'Method Failure'],
+  [425, 'Unordered Collection'],
+  [426, 'Upgrade Required'],
+  [428, 'Precondition Required'],
+  [429, 'Too Many Requests', function(config) {
+    this.retryAfter = config.retryAfter;
+  }],
+  [431, 'Request Header Fields Too Large'],
+  [440, 'Login Timeout'],
+  [444, 'No Response'],
+  [449, 'Retry With', function(config) {
+    this.parameters = config.parameters;
+  }],
+  [450, 'Blocked By Windows Parental Controls'],
+  [451, 'Unavailable For Legal Reasons'],
+  [451, 'Redirect', function(config) {
+    this.location = config.location;
+  }],
+  [494, 'Request Header Too Large'],
+  [495, 'Cert Error'],
+  [496, 'No Cert'],
+  [497, 'HTTP To HTTPS'],
+  [499, 'Client Closed Request'],
+  [500, 'Internal Server Error'],
+  [501, 'Not Implemented'],
+  [502, 'Bad Gateway'],
+  [503, 'Service Unavailable', function(config) {
+    this.retryAfter = config.retryAfter;
+  }],
+  [504, 'Gateway Timeout'],
+  [505, 'HTTP Version Not Supported'],
+  [506, 'Variant Also Negotiates'],
+  [507, 'Insufficient Storage'],
+  [508, 'Loop Detected'],
+  [509, 'Bandwidth Limit Exceeded'],
+  [510, 'Not Extended'],
+  [511, 'Network Authentication Required'],
+  [520, 'Origin Error'],
+  [522, 'Connection Timed Out'],
+  [523, 'Proxy Declined Request'],
+  [524, 'A Timeout Occured'],
+  [598, 'Network Read Timeout Error'],
+  [599, 'Network Connect Timeout Error']
+].forEach(spread(function(status, title, fn) {
+  var httperr = createFactory(status, title, fn);
+  var exportName = snakeCase(simplify(title));
+  if (!exports[exportName]) {
+    exports[exportName] = httperr;
+  }
+  if (!exports[status]) {
+    exports[status] = httperr;
+  }
+}));
